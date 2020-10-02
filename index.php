8fa9ae002cfe4c361bfe8119fac94077a959a65d
Quentin <win.moi@hotmail.com>
2020-10-02 15:49:23 +0200

    Add database

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ use Controllers\Authentification\Security;
 use Controllers\Autoloader;
 
 define ('ROOT', dirname(__DIR__) . '/Todo/');
-	require ROOT . 'Controllers/Autoloader.php';
-	Autoloader::register();
+require ROOT . 'Controllers/Autoloader.php';
+Autoloader::register();
 
 $s = new Security();
 echo $s->Hash("coucou");
+
+new \Controllers\Database\Database();
