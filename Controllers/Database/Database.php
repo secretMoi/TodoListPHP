e95ec420f7947601c3259df013db557e56c5ea95
Quentin <win.moi@hotmail.com>
2020-10-02 17:38:08 +0200

    Add sql

--- Controllers/Database/Database.php
+++ Controllers/Database/Database.php
@@ -4,19 +4,16 @@ namespace Controllers\Database;
 
 class Database
 {
-	private $host = "192.168.1.124";
-	private $port = "1433";
+	private $host = "localhost";
+	private $port = "3306";
 	private $name = "todo";
-	private $user = "sa";
-	private $password = "Nax2J,/nwdbLQGj";
-
-	//private $connection = "mysql:host=".$this->port.";dbname=".$this->name.";port=".$this->port,$this->user,$this->password;
+	private $user = "root";
+	private $password = "";
 
 	private $pdo;
 
 	public function __construct()
 	{
-		//$this->pdo = new \PDO("mysql:host={$this->host};dbname={$this->name};port={$this->port}",$this->user,$this->password);
-		$this->pdo = new \PDO("sqlsrv:server={$this->host}:{$this->port};Database={$this->name}",$this->user,$this->password);
+		$this->pdo = new \PDO("mysql:host={$this->host};dbname={$this->name};port={$this->port}",$this->user,$this->password);
 	}
 }
