20cfa18bac134eebe3c7127995810119812ed1e0
Quentin <win.moi@hotmail.com>
2020-10-07 18:41:48 +0200

    Ajout fonction WHERE et WHEREID RequestBuilder.php

--- Views/Pages/Login/log.php
+++ Views/Pages/Login/log.php
@@ -1,27 +1,19 @@
-<!--<div class="d-flex justify-content-center">
-    <div class="form-group d-flex justify-content-center" style="max-width: 30rem;">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group d-flex justify-content-center" style="max-width: 30rem;">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-</div>-->
 <form style="max-width: 30rem; margin-left: auto; margin-right: auto;">
     <div class="form-group">
         <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <input type="email" class="form-control" id="exampleInputEmail1" name="mail" aria-describedby="emailHelp" placeholder="Enter email">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
+    
     <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
     </div>
+
     <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="log">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+
+    <button type="submit" class="btn btn-primary" name="log">Se connecter</button>
 </form>
