12623c77365c7e0dab1aa22cc02451e8269fe164
sebas <sebastien.damsin@gmail.com>
2020-10-07 21:14:45 +0200

    Mise en forme btn "Se connecter" -> log.php

--- Views/Pages/Login/log.php
+++ Views/Pages/Login/log.php
@@ -1,4 +1,4 @@
-<form style="max-width: 30rem; margin-left: auto; margin-right: auto;">
+<form style="max-width: 30rem; margin: 100px auto;">
     <div class="form-group">
         <label for="exampleInputEmail1">Email address</label>
         <input type="email" class="form-control" id="exampleInputEmail1" name="mail" aria-describedby="emailHelp" placeholder="Enter email">
@@ -15,5 +15,5 @@
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
     </div>
 
-    <button type="submit" class="btn btn-primary" name="log">Se connecter</button>
+    <button type="submit" class="btn btn-primary" name="log" style="margin: 10px auto;">Se connecter</button>
 </form>
